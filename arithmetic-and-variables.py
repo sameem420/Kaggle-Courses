a40ac3e975a502e85c2ffb7e98cc209a3f4325c6
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-06-10 14:55:14 +0400

    :heavy_check_mark: Make calculations, and define and modify variables.

---
+++ arithmetic-and-variables.py
@@ -0,0 +1,69 @@
+# Printing
+print("Hello World!")
+
+# Arithmetic
+
+print(1+2)
+
+print(9-5)
+
+# Operation | Symbol | Example
+# Addition | + | 1 + 2 = 3
+# Subtraction | - | 5 - 4 = 1
+# Multiplication | * | 2 * 4 = 8
+# Division | / | 6 / 3 = 2
+# Exponent | ** | 3 ** 2 = 9
+
+print(((1 + 3) * (9 - 2) / 2) ** 2)
+
+# Comments
+
+# Multiply 3 by 2
+print(3 * 2)
+
+# Variables
+
+# Create a variable called test_var and give it a value of 4+5
+test_var = 4+5
+
+# Print the value of test_var
+print(test_var)
+
+# Manipulating variables
+# Set the value of a new variable to 3
+my_var = 3
+
+# Print the value assigned to my_var
+print(my_var)
+
+# Change the value of the variable to 100
+my_var = 100
+
+# Print the new value assigned to my_var
+print(my_var)
+
+# Increase the value by 3
+my_var = my_var + 3
+
+# Print the value assigned to my_var
+print(my_var)
+
+# Using multiple variables
+
+# Create variables
+num_years = 4
+days_per_year = 365 
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+
+# Calculate number of seconds in four years
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
+
+# Update to include leap years
+days_per_year = 365.25
+
+# Calculate number of seconds in four years
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
